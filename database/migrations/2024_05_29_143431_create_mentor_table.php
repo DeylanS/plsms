5f13bac327bfb740fec5024a7dfeff8d59aeb563
nissssab <hanis.sabrina2002@gmail.com>
2024-05-29 22:38:54 +0800

    add mentor

---
+++ database/migrations/2024_05_29_143431_create_mentor_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mentor', function (Blueprint $table) {
+            $table->id('Mentor_ID');
+            $table->foreign('User_ID')->reference('User_ID')->on('user')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('Mentor_Staff');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mentor');
+    }
+};
